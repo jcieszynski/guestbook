bb56b7504751c387f579d87f929c1e24608b8775
Jacek Cieszyński <jacox520@gmail.com>
2020-10-06 19:52:39 +0200

    One to many relation between comment and Conference photoFilename optional field added to comment

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -37,6 +37,17 @@ class Comment
      */
     private $createdAt;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=Conference::class, inversedBy="comments")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $conference;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $photoFilename;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -86,6 +97,30 @@ class Comment
     public function setCreatedAt(\DateTimeInterface $createdAt): self
     {
         $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getConference(): ?Conference
+    {
+        return $this->conference;
+    }
+
+    public function setConference(?Conference $conference): self
+    {
+        $this->conference = $conference;
+
+        return $this;
+    }
+
+    public function getPhotoFilename(): ?string
+    {
+        return $this->photoFilename;
+    }
+
+    public function setPhotoFilename(?string $photoFilename): self
+    {
+        $this->photoFilename = $photoFilename;
 
         return $this;
     }

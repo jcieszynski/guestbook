7f1599f1fba3284ba257c19217e57492f9d01d3a
Jacek Cieszyński <jacox520@gmail.com>
2020-11-18 16:26:12 +0100

    Dependency injection

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -13,15 +13,21 @@ use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
     /**
      * @Route("/", name="homepage")
-     * @param Environment $twig
      * @param ConferenceRepository $conferenceRepository
      * @return Response
      */
-    public function index(Environment $twig, ConferenceRepository $conferenceRepository)
+    public function index(ConferenceRepository $conferenceRepository)
     {
-        return new Response($twig->render('conference/index.html.twig',
+        return new Response($this->twig->render('conference/index.html.twig',
             ['conferences' => $conferenceRepository->findAll(),
                 'controller_name' => 'Conference']));
     }
@@ -29,16 +35,15 @@ class ConferenceController extends AbstractController
     /**
      * @Route("/conference/{id}",name="conference")
      * @param Request $request
-     * @param Environment $twig
      * @param Conference $conference
      * @param CommentRepository $commentRepository
      * @return Response
      */
-    public function show(Request $request, Environment $twig, Conference $conference, CommentRepository $commentRepository)
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository)
     {
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
-        return new Response($twig->render('conference/show.html.twig', [
+        return new Response($this->twig->render('conference/show.html.twig', [
             'conference' => $conference,
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,

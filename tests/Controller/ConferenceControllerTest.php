d3983e6972847ba1475e4adc9442ce0f160806c3
Jacek Cieszyński <jacox520@gmail.com>
2020-12-07 19:51:56 +0100

    functional test package added

---
+++ tests/Controller/ConferenceControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ConferenceControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        self::assertResponseIsSuccessful();
+        self::assertSelectorTextContains('h2', 'Give your feedback');
+    }
+}

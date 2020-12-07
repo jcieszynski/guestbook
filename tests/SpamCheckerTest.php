aa70d8c8fe87140896173f22c8b4ff59a799f81e
Jacek Cieszyński <jacox520@gmail.com>
2020-12-07 19:30:07 +0100

    added: testSpamScore getComments data provider

--- tests/SpamCheckerTest.php
+++ tests/SpamCheckerTest.php
@@ -7,6 +7,7 @@ use App\SpamChecker;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpClient\MockHttpClient;
 use Symfony\Component\HttpClient\Response\MockResponse;
+use Symfony\Contracts\HttpClient\ResponseInterface;
 
 class SpamCheckerTest extends TestCase
 {
@@ -23,5 +24,38 @@ class SpamCheckerTest extends TestCase
         $this->expectException(\RuntimeException::class);
         $this->expectExceptionMessage('Unable to check for spam: invalid (Invalid key).');
         $checker->getSpamScore($comment, $context);
+    }
+
+    /**
+     * @dataProvider getComments
+     * @param int $expectedScore
+     * @param ResponseInterface $response
+     * @param Comment $comment
+     * @param array $context
+     */
+
+    public function testSpamScore(int $expectedScore, ResponseInterface $response, Comment $comment, array $context): void
+    {
+        $client = new MockHttpClient([$response]);
+        $checker = new SpamChecker($client, 'abcde');
+
+        $score = $checker->getSpamScore($comment, $context);
+        self::assertSame($expectedScore, $score);
+    }
+
+    public function getComments(): iterable
+    {
+        $comment = new Comment();
+        $comment->setCreatedAtValue();
+        $context = [];
+
+        $response = new MockResponse('', ['response_headers' => ['x-akismet-pro-tip: discard']]);
+        yield 'blatant_spam' => ['2', $response, $comment, $context];
+
+        $response = new MockResponse('true');
+        yield 'spam' => ['1', $response, $comment, $context];
+
+        $response = new MockResponse('false');
+        yield 'ham' => [0, $response, $comment, $context];
     }
 }

e9925201d37bbdda085981789703b7cbe31470b8
Jacek Cieszyński <jacox520@gmail.com>
2020-12-11 23:58:49 +0100

    Fixture data

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Comment;
+use App\Entity\Conference;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $amsterdam = new Conference();
+        $amsterdam->setCity('Amsterdam');
+        $amsterdam->setYear('2019');
+        $amsterdam->setIsInternational(true);
+        $manager->persist($amsterdam);
+
+        $paris = new Conference();
+        $paris->setCity('Paris');
+        $paris->setYear('2019');
+        $paris->setIsInternational(false);
+        $manager->persist($paris);
+
+        $comment1 = new Comment();
+        $comment1->setConference($amsterdam);
+        $comment1->setAuthor('Jack');
+        $comment1->setEmail('test@test.com');
+        $comment1->setText('Good conference');
+        $manager->persist($comment1);
+
+        $manager->flush();
+    }
+}

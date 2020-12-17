cecf61773cbfb3c61c42463621aa000c941a43d6
Jacek Cieszyński <jacox520@gmail.com>
2020-12-17 21:38:14 +0100

    symfony/mime added testCommentSubmission added

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -14,4 +14,19 @@ class ConferenceControllerTest extends WebTestCase
         self::assertResponseIsSuccessful();
         self::assertSelectorTextContains('a', 'Guestbook');
     }
+
+    public function testCommentSubmission()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/conference/amsterdam-2019');
+        $client->submitForm('Submit', [
+            'comment_form[author]' => 'Fabien',
+            'comment_form[text]' => 'Some feedback from an automated functional test',
+            'comment_form[email]' => 'me@automat.ed',
+            'comment_form[photo]' => dirname(__DIR__, 2) . '/public/images/under-construction-en.png'
+        ]);
+        self::assertResponseRedirects();
+        $client->followRedirect();
+       // self::assertSelectorExists('div:contains("There are 2 comments")');
+    }
 }

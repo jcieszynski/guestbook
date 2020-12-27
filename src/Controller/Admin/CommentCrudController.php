587927fb02d18c75ba78fd379d86ab7c525f14e5
Jacek Cieszyński <jacox520@gmail.com>
2020-12-27 18:21:40 +0100

    Table comment altered and added column added ConferenceControllerTest updated state field added with submitted option as default Fixtures updated CommentCrudController state field added and setted upload dir to image field

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -5,11 +5,14 @@ namespace App\Controller\Admin;
 use App\Entity\Comment;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use http\Env;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
 
 class CommentCrudController extends AbstractCrudController
 {
@@ -26,8 +29,9 @@ class CommentCrudController extends AbstractCrudController
             EmailField::new('email'),
             DateTimeField::new('createdAt'),
             TextEditorField::new('text'),
-            ImageField::new('photoFilename'),
+            ImageField::new('photoFilename')->setUploadDir('/public/uploads/photos'),
             AssociationField::new('conference')->autocomplete(),
+            ChoiceField::new('state')->setChoices(['submitted' => 'submitted'])
         ];
     }
 
--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace App\Tests\Controller;
 
+use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Panther\PantherTestCase;
 
 class ConferenceControllerTest extends PantherTestCase
@@ -22,10 +24,14 @@ class ConferenceControllerTest extends PantherTestCase
         $client->submitForm('Submit', [
             'comment_form[author]' => 'Fabien',
             'comment_form[text]' => 'Some feedback from an automated functional test',
-            'comment_form[email]' => 'me@automat.ed',
+            'comment_form[email]' => $email = 'me@automat.ed',
             'comment_form[photo]' => dirname(__DIR__, 2) . '/public/images/under-construction-en.png'
         ]);
         self::assertResponseRedirects();
+        //simulate comment validation
+        $comment = self::$container->get(CommentRepository::class)->findOneByEmail($email);
+        $comment->setState('published');
+        self::$container->get(EntityManagerInterface::class)->flush();
         $client->followRedirect();
         // self::assertSelectorExists('div:contains("There are 2 comments")');
     }
